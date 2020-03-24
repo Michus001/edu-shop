c219959346fd7c0704f391695abc75816c0b7df1
tomas <tomas@michalicka.com>
2020-03-24 12:41:18 +0100

    init project

---
+++ src/main/java/cz/vse/java/edushop/EduShopApplication.java
@@ -0,0 +1,13 @@
+package cz.vse.java.edushop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/vse/java/edushop/EduShopApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.vse.java.edushop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
